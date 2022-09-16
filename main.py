979e269ae8371f53cfb4d73f225df277b70a09bc
yehancc <91064433+yehancc@users.noreply.github.com>
2022-09-16 16:52:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -161,10 +161,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": wind_dir,
                 "color": get_color()
             },
-            "love_day": {
-                "value": love_days,
-                "color": get_color()
-            },
+#             "love_day": {
+#                 "value": love_days,
+#                 "color": get_color()
+#             },
             "note_en": {
                 "value": note_en,
                 "color": get_color()
